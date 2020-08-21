89c976ebb5c164e08452af4be6c8388b9ecf28ed
qinhuaigan <624518812@qq.com>
2020-08-21 19:54:52 +0800

    引入 "腾讯地图"

---
+++ json/locationData.js
@@ -0,0 +1,102 @@
+var data = {
+  "01": {
+    "longitude": 109.430407,
+    "latitude": 24.384296
+  },
+  "02": {
+    "longitude": 109.491851,
+    "latitude": 24.346228
+  },
+  "03": {
+    "longitude": 109.491851,
+    "latitude": 24.346228
+  }, 
+  "04": {
+    "longitude": 109.444492,
+    "latitude": 24.384296
+  },
+  "05": {
+    "longitude": 109.458118,
+    "latitude": 24.341609
+  },
+  "06": {
+    "longitude": 109.434163,
+    "latitude": 24.367896
+  },
+  "07": {
+    "longitude": 109.270946,
+    "latitude": 24.36911
+  },
+  "08": {
+    "longitude": 109.270811,
+    "latitude": 24.368633
+  },
+  "09": {
+    "longitude": 109.359543,
+    "latitude": 24.303232
+  },
+  "10": {
+    "longitude": 109.351207,
+    "latitude": 24.310608
+  },
+  "11": {
+    "longitude": 109.394901,
+    "latitude": 24.360391
+  },
+  "12": {
+    "longitude": 109.433756,
+    "latitude": 24.235451
+  },
+  "13": {
+    "longitude": 109.434804,
+    "latitude": 24.302166
+  },
+  "14": {
+    "longitude": 109.441488,
+    "latitude": 24.282075
+  },
+  "15": {
+    "longitude": 109.53721,
+    "latitude": 24.3641
+  },
+  "16": {
+    "longitude": 109.51795,
+    "latitude":24.379373
+  },
+  "17": {
+    "longitude": 109.535176,
+    "latitude": 24.379406
+  },
+  "18": {
+    "longitude": 109.512539,
+    "latitude": 24.286485
+  },
+  "19": {
+    "longitude": 109.495076,
+    "latitude": 24.319349
+  },
+  "20": {
+    "longitude": 109.466016,
+    "latitude": 24.329787
+  },
+  "21": {
+    "longitude": 109.441569,
+    "latitude": 24.344584
+  },
+  "22": {
+    "longitude": 109.360534,
+    "latitude": 24.281819
+  },
+  "23": {
+    "longitude": 109.352524,
+    "latitude": 24.313036
+  },
+  "24": {
+    "longitude": 109.448378,
+    "latitude": 24.30281
+  }
+}
+
+export default {
+  data
+}
---
+++ pages/mapPage/mapPage.js
@@ -0,0 +1,112 @@
+// pages/mapPage/mapPage.js
+import locationData from '../../json/locationData.js'
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    longitude: 113.324520, // 中心经度
+    latitude: 23.099994, // 中心维度
+    markers: []
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    const that = this
+    wx.getSetting({
+      success(res) {
+        if (!res.authSetting['scope.userLocation']) {
+          // 用户未授权，请求用户授权
+          wx.authorize({
+            scope: 'scope.userLocation',
+            success () {
+              // 用户已经同意小程序获取地理位置
+              wx.getLocation({
+                altitude: 'altitude',
+                success: function (res) {
+                  that.setData({
+                    longitude: res.longitude,
+                    latitude: res.latitude,
+                    markers: [locationData.data[options.id]]
+                  })
+                  wx.openLocation(locationData.data[options.id])
+                },
+                fail: function (err) {
+                  console.log('获取地理位置失败 err', err)
+                }
+              })
+            }
+          })
+        } else {
+          // 用户已授权 "地理位置"
+          wx.getLocation({
+            altitude: 'altitude',
+            success: function (res) {
+              that.setData({
+                longitude: res.longitude,
+                latitude: res.latitude,
+                markers: [locationData.data[options.id]]
+              })
+              wx.openLocation(locationData.data[options.id])
+            },
+            fail: function (err) {
+              console.log('获取地理位置失败 err', err)
+            }
+          })
+        }
+      }
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
