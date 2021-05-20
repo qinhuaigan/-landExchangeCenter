3b594de24a097012e1e9504145528d165865d68b
lhy <haiyan01100520@163.com>
2021-05-20 14:04:04 +0800

    提交地图json

--- json/locationData.js
+++ json/locationData.js
@@ -4,48 +4,48 @@ var data = {
     "latitude": 24.385889
   },
   "02": {
-    "longitude": 109.471134,
-    "latitude": 24.333492
+    "longitude": 109.462795,
+    "latitude": 24.332332
   },
   "03": {
-    "longitude": 109.471134,
-    "latitude": 24.333492
+    "longitude": 109.436426,
+    "latitude": 24.368669
   }, 
   "04": {
-    "longitude": 109.446377,
-    "latitude": 24.386557
+    "longitude": 109.470529,
+    "latitude": 24.331866
   },
   "05": {
-    "longitude": 109.471134,
-    "latitude": 24.333492
+    "longitude": 109.372216,
+    "latitude": 24.313966
   },
   "06": {
-    "longitude": 109.435966,
-    "latitude": 24.369548
+    "longitude": 109.363773,
+    "latitude": 24.363448
   },
   "07": {
-    "longitude": 109.353178,
-    "latitude": 24.358735
+    "longitude": 109.356104,
+    "latitude": 24.318528
   },
   "08": {
-    "longitude": 109.353178,
-    "latitude": 24.358735
+    "longitude": 109.387394,
+    "latitude": 24.280585
   },
   "09": {
-    "longitude": 109.350521,
-    "latitude": 24.271969
+    "longitude": 109.359617,
+    "latitude": 24.35342
   },
   "10": {
-    "longitude": 109.350135,
-    "latitude": 24.305602
+    "longitude": 109.401665,
+    "latitude": 24.403134
   },
   "11": {
-    "longitude": 109.39444,
-    "latitude": 24.361888
+    "longitude": 109.394497,
+    "latitude": 24.360947
   },
   "12": {
-    "longitude": 109.431719,
-    "latitude": 24.235172
+    "longitude": 109.53893,
+    "latitude": 24.383052
   },
   "13": {
     "longitude": 109.450548,
@@ -64,36 +64,52 @@ var data = {
     "latitude":24.38235
   },
   "17": {
-    "longitude": 109.549715,
-    "latitude": 24.402923
+    "longitude": 109.550126,
+    "latitude": 24.403956
   },
   "18": {
-    "longitude": 109.514313,
-    "latitude": 24.386266
+    "longitude": 109.538043,
+    "latitude": 24.340916
   },
   "19": {
-    "longitude": 109.480672,
-    "latitude": 24.294928
+    "longitude": 109.538764,
+    "latitude": 24.427656
   },
   "20": {
-    "longitude": 109.482594,
-    "latitude": 24.336733
+    "longitude": 109.512138,
+    "latitude": 24.387462
   },
   "21": {
-    "longitude": 109.441486,
-    "latitude": 24.343831
+    "longitude": 109.504642,
+    "latitude": 24.338176
   },
   "22": {
-    "longitude": 109.368417,
-    "latitude": 24.292309
+    "longitude": 109.471459,
+    "latitude": 24.286821
   },
   "23": {
-    "longitude": 109.353333,
-    "latitude": 24.311476
+    "longitude": 109.469806,
+    "latitude": 24.282736
   },
   "24": {
-    "longitude": 109.448446,
-    "latitude": 24.302823
+    "longitude": 109.49663,
+    "latitude": 24.314609
+  },
+  "25": {
+    "longitude": 109.371882,
+    "latitude": 24.269115
+  },
+  "26": {
+    "longitude": 109.335846,
+    "latitude": 24.244167
+  },
+  "27": {
+    "longitude": 109.332491,
+    "latitude": 24.24418
+  },
+  "28": {
+    "longitude": 109.324573,
+    "latitude": 24.254546
   }
 }
 
