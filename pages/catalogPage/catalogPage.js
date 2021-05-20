ebf3dd45877419e5dfa62589b5447b70f25c05db
qinhuaigan <624518812@qq.com>
2021-05-20 10:00:09 +0800

    更新1-13页地块信息

--- pages/catalogPage/catalogPage.js
+++ pages/catalogPage/catalogPage.js
@@ -30,7 +30,7 @@ Page({
         name: '07.柳南区——瑞龙路东侧工人医院新院南侧',
         link: '../detailPage7/detailPage7'
       }, {
-        name: '08.柳南区——旧机场片区航岭路西侧国联运输公司地块',
+        name: '08.柳南区——旧机场片区航岭路西侧地块',
         link: '../detailPage8/detailPage8'
       }, {
         name: '09.柳南区——瑞龙路东侧河西桃花源西北侧部分地块',
@@ -42,46 +42,55 @@ Page({
         name: '11.柳北区——北雀路西片B-1-4、B-2-2地块',
         link: '../detailPage11/detailPage11'
       }, {
-        name: '12.柳东新区——柳东新区九子岭西侧A03、A07、A10地块',
+        name: '12.柳北区——东外环北侧地块',
         link: '../detailPage12/detailPage12'
       }, {
-        name: '13.柳东新区——柳东新区曙光大道南侧L-19-27地块',
+        name: '13.鱼峰区——都乐路北侧部分地块',
         link: '../detailPage13/detailPage13'
       }, {
-        name: '14.柳东新区——职教园集中教学区B-2-6、B-2-9地块',
+        name: '14.鱼峰区——西江路南侧171号、262号、262号地块',
         link: '../detailPage14/detailPage14'
       }, {
-        name: '15.柳东新区——曙光大道西段南片区B-6-2、B-6-3地块',
+        name: '15.柳东新区——柳东新区九子岭西侧A03、A07、A10地块',
         link: '../detailPage15/detailPage15'
       }, {
-        name: '16.柳东新区——柳东新区官塘物流港H-4-1地块',
+        name: '16.柳东新区——柳东新区曙光大道南侧L-19-27地块',
         link: '../detailPage16/detailPage16'
       }, {
-        name: '17.柳东新区——滨江东片区东环城大道与纬六路交叉口西南侧部分',
+        name: '17.柳东新区——职教园集中教学区B-2-6、B-2-9地块',
         link: '../detailPage17/detailPage17'
       }, {
-        name: '18.北部生态新区——阳和工业新区A-3-1、A-4-1、A-5-2地块',
+        name: '18.柳东新区——曙光大道西段南片区B-6-2、B-6-3地块',
         link: '../detailPage18/detailPage18'
       }, {
-        name: '19.北部生态新区——阳和工业新区K-8-1. K-9-1、K-12-2、K-13-1 地块',
+        name: '19.柳东新区——柳东新区官塘物流港H-4-1地块',
         link: '../detailPage19/detailPage19'
       }, {
-        name: '20.北部生态新区——阳和工业新区K-10-1地块',
+        name: '20.柳东新区——滨江东片区东环城大道与纬六路交叉口西南侧部分',
         link: '../detailPage20/detailPage20'
       }, {
-        name: '21.北部生态新区——古亭片区E-8-1-1、E-8-1-2地块（鼓山公园北侧）',
+        name: '21.北部生态新区——阳和工业新区A-3-1、A-4-1、A-5-2地块',
         link: '../detailPage21/detailPage21'
       }, {
-        name: '22.柳江区——南环路原东风柳汽二三生产基地部分地块',
+        name: '22.北部生态新区——阳和工业新区K-8-1、K-9-1、K-12-2、K-13-1地块',
         link: '../detailPage22/detailPage22'
       }, {
-        name: '23.柳江区——柳江市民广场东面、南一路和南二路之间地块',
+        name: '23.北部生态新区——阳和工业新区K-10-1地块',
         link: '../detailPage23/detailPage23'
       }, {
-        name: '24.柳江区——柳江大道以南沿江路以西地块',
+        name: '24.北部生态新区——古亭片区E-8-1-1、E-8-1-2地块（鼓山公园北侧）',
         link: '../detailPage24/detailPage24'
       }, {
-        name: '25.柳江区——柳江大道C-1-2地块',
+        name: '25.柳江区——南环路原东风柳汽二三生产基地',
+        link: '../detailPage25/detailPage25'
+      }, {
+        name: '26.柳江区——柳江市民广场东面、南一路和南二路之间',
+        link: '../detailPage25/detailPage25'
+      }, {
+        name: '27.柳江区——柳江大道以南沿江路以西地块',
+        link: '../detailPage25/detailPage25'
+      }, {
+        name: '28.柳江区——柳江大道C-1-2地块',
         link: '../detailPage25/detailPage25'
       }]
     }]
